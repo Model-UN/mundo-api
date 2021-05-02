b82bfc6f3385048e19a59cc518e0066a6b3dcab0
James He <hejames98@gmail.com>
2021-05-02 19:07:15 -0400

    Add swagger

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,20 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const core_1 = require('@nestjs/core');
-const platform_fastify_1 = require('@nestjs/platform-fastify');
-const app_module_1 = require('./app.module');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const platform_fastify_1 = require("@nestjs/platform-fastify");
+const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
-  const app = await core_1.NestFactory.create(
-    app_module_1.AppModule,
-    new platform_fastify_1.FastifyAdapter(),
-  );
-  await app.listen(3000, '0.0.0.0');
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_fastify_1.FastifyAdapter());
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('MUNDO Conference API')
+        .setDescription('ReST-ful data access layer for MUNDO Conferences.')
+        .setVersion('1.0')
+        .addTag('model-un')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,31 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  // Initialize Nest-Fastify app
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  /* Swagger Config
+   * For more information on OpenAPI, fastify-swagger, @nestjs/swagger, refer to
+   * the Nest documentation: https://docs.nestjs.com/openapi/introduction
+   */
+  const config = new DocumentBuilder()
+    .setTitle('MUNDO Conference API')
+    .setDescription('ReST-ful data access layer for MUNDO Conferences.')
+    .setVersion('1.0')
+    .addTag('model-un')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Set listener at localhost:3000
   await app.listen(3000, '0.0.0.0');
 }
+// Bootstrap Nest App
+// noinspection JSIgnoredPromiseFromCall
 bootstrap();
