511ca5fc7737d0219c5ecb2e36cbb3f931dab72a
James He <hejames98@gmail.com>
2021-06-21 01:59:59 -0500

    Remove unused file

--- src/entities/ormconfig.ts
+++
@@ -1,6 +0,0 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { EntityConfig } from './entity.config';
-import { SnakeNamingStrategy } from './snakeNamingStrategy';
-
-export const typeormConfig: TypeOrmModuleOptions = {
-};
