cb3baf0b86e8f46ac461258ff92df1ff8fb8c29c
James He <hejames98@gmail.com>
2021-08-20 10:18:43 -0500

    Change ec2 username

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cd /home/ec2-user/mundo-backend
+cd /home/ubuntu/mundo-backend
 sudo aws s3 cp s3://mundo-api-secrets/prod/.env ./.env
 sudo docker-compose build --no-cache
 sudo docker-compose up -d
